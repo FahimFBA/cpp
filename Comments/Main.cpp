9d745b5ad27a2767fd5b1e9adaf225e060bb1aa9
FahimFBA <fahimbinamin@gmail.com>
2023-01-18 01:04:22 +0600

    Comment in C++

---
+++ Comments/Main.cpp
@@ -0,0 +1,16 @@
+//  This is a code for printing "Hello World!" - Single Line Comment
+//  Ctrl + /
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // asterisk 
+    // This
+    // Is asdfhausnfas w395r2y3w589t3h6
+    // Also
+    // A
+    // Comment
+    
+    cout << "Hello World!" << endl;
+}
