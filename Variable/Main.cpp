24f7f5b3dfb23a25a0a522b9a1bf228f700fd334
FahimFBA <fahimbinamin@gmail.com>
2023-01-24 13:21:13 +0600

    variable

---
+++ Variable/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Camelcase convention 
+    // First_Name
+    // FirstName
+    int x; // declaration
+    x = 5; // initialization
+    // int y;
+    // y = 56;
+    int y = 56;
+}
