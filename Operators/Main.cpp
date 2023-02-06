b08869c63d9e8e6cfbcef420578c04cf109f3f87
FahimFBA <fahimbinamin@gmail.com>
2023-02-06 12:15:41 +0600

    operators

---
+++ Operators/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 20;
+    int b = 15;
+
+    // Addition
+    int sum = a + b;
+    cout << "The summation is: " << sum << endl;
+
+    // Subtraction
+    int sub = a - b;
+    cout << "The subtraction is: " << sub << endl;
+
+    // Multiplication
+    int multi = a * b;
+    cout << "The multiplication is: " << multi << endl;
+
+    // Division
+    int div = a / b;
+    cout << "The division is: " << div << endl;
+
+    // Modulus
+    int modulo = a % b;
+    cout << "The modulus is: " << modulo << endl;
+
+}
