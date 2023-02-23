b7280ee684de5ff81ae154fc10a460036a0e5859
FahimFBA <fahimbinamin@gmail.com>
2023-02-24 01:17:00 +0600

    code added

---
+++ String/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //    cout << "Hello World!";
+    //    return 0;
+    //    string myString = "Hello World!";
+    //    cout << myString;
+
+    string FirstName = "Md. Fahim\n";
+    string MiddleName = "Bin\n";
+    string LastName = "Amin";
+    cout << FirstName;
+    cout << MiddleName;
+    cout << LastName;
+}
---
+++ UserInput/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //    int a = 5;
+    //    int b = 10;
+    //    cout << a << endl;
+    //    cout << b << endl;
+    int a, b;
+    cout << "Enter the first integer value: ";
+    cin >> a;
+    cout << "Enter the second integer value: ";
+    cin >> b;
+    cout << "<- The value for a and b ->";
+    cout << endl
+         << a << " " << b;
+}
