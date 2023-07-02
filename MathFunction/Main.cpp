ff7f489b3ce339fef02ad28e3bf7c8d0738041e5
FahimFBA <fahimbinamin@gmail.com>
2023-07-03 01:22:21 +0600

    Math function

---
+++ MathFunction/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // int MaxNumber = max(15, 5);
+    // cout << MaxNumber;
+    // int MinNumber = min(5, 15);
+    // cout << MinNumber;
+    // cout << max(5.55, 5.56);
+    // cout << min(-5, -800);
+    // cout << sqrt(64);
+    cout << round(5.499999);
+}
