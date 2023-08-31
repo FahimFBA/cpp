59f4817a7e046de5fbeedca5c04f1da5d0c2ae26
FahimFBA <fahimbinamin@gmail.com>
2023-08-31 17:27:53 +0600

    bool

---
+++ Boolean/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool isRaining = false;
+    // YES or NO = 1 OR 0
+    // TRUE or FALSE = 1 OR 0
+    // ON or OFF = 1 OR 0
+    cout << isRaining << endl;
+    isRaining = true;
+    cout << isRaining << endl;
+    bool hasElectricity = 1;
+    cout << hasElectricity << endl;
+    hasElectricity = 0;
+    cout << hasElectricity << endl;
+}
---
+++ Boolean/Main2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 89;
+    int b = 12;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    int myAge = 25;
+    int minCriteriaAge = 30;
+    cout << (myAge >= minCriteriaAge) << endl;
+    int hisAge = 50;
+    cout << (hisAge >= minCriteriaAge) << endl;
+}
