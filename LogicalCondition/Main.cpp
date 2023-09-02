436a6426323cbded15f33966f0b3c92d86ca7872
FahimFBA <fahimbinamin@gmail.com>
2023-09-02 22:55:01 +0600

    logical condition

---
+++ LogicalCondition/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    x = 20;
+    y = 30;
+    // >
+    // if (y > x)
+    //     cout << "Y is greater than x" << endl;
+    // if (x > y)
+    //     cout << "X is greater than Y" << endl;
+
+    // <
+    // if (x < y)
+    //     cout << "X is smaller than Y" << endl;
+    // if (x < y)
+    //     cout << "X is smaller than Y" << endl;
+
+    // >=
+    // y = 40;
+    // x = 50;
+    // if (x >= y)
+    //     cout << "X is greater than or equal to Y" << endl;
+
+    // <=
+    // if (x <= y)
+    //     cout << "X is less than or equal to Y" << endl;
+
+    // ==
+    // if (x == y)
+    //     cout << "X is equal to Y" << endl;
+
+    // !=
+    if (x != y)
+        cout << "X is not equal to Y" << endl;
+}
